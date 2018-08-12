cf5e3f2b31632970b788eaa37f2672918315a112
ShivaShankariG <shivguru06@gmail.com>
2018-08-12 18:55:49 +0530

    alignment instructions db page-transtion

--- bid1.js
+++ bid1.js
@@ -79,10 +79,10 @@ function checkAns() {
     //alert(ans)
     var id = getQues.qno;
     if (ans == easy[id][1]) {
-        if (id >= 0 && id <= 5) {
+        if (id >= 0 && id <= 9) {
             coins = coins + 10;
         }
-        else if (id >= 6 && id <= 8) {
+        else if (id >= 10 && id <= 19) {
             coins = coins + 20;
         }
         else {
@@ -99,7 +99,7 @@ function checkAns() {
     $('#dialog').hide()
     $('#transparent-bg').hide()
     // document.getElementById("coins").value = coins;
-    //check()
+    check()
 }
 function check() {
     if (document.getElementById("coins").value >= 50) {
@@ -129,18 +129,33 @@ function check() {
 }
 function getQues(id) {
     //  $("#tableManager").modal('show');
+
     getQues.qno = id;
     var coins = $("#coins").val()
     alert("qn no:" + id)
-    if (id >= 0 && id <= 5) {
+    if (id >= 0 && id <= 9) {
         coins = coins - 50;
+        if (typeof getQues.eCount == 'undefined')
+            getQues.eCount = 1;
+        else
+            getQues.eCount += 1;
     }
-    else if (id >= 6 && id <= 8) {
+    else if (id >= 10 && id <= 19) {
         coins = coins - 30;
+        if (typeof getQues.mCount == 'undefined')
+            getQues.mCount = 1;
+        else
+            getQues.mCount += 1;
     }
     else {
         coins = coins - 10;
+        if (typeof getQues.hCount == 'undefined')
+            getQues.hCount = 1;
+        else
+            getQues.hCount += 1;
+
     }
+
     document.getElementById("coins").value = coins;
     $('#transparent-bg').show();
     var img = document.createElement('img');
@@ -151,7 +166,7 @@ function getQues(id) {
 }
 
 document.getElementById('timer').innerHTML =
-    01 + ":" + 00;
+    30 + ":" + 01;
 startTimer();
 
 function startTimer() {
@@ -161,13 +176,18 @@ function startTimer() {
     var s = checkSecond((timeArray[1] - 1));
     if (s == 59) {
         m = m - 1
-        if (m > 0) m = "time expired";
+    }
+    if (m == 0 && s == 00) {
+
+        $('#end-dialog').show()
+        $('#transparent-bg').show()
     }
     //if(m<0){alert('timer completed')}
 
     document.getElementById('timer').innerHTML =
         m + ":" + s;
-    setTimeout(startTimer, 1000);
+    if (!(m == 0 && s == 00))
+        setTimeout(startTimer, 1000);
 }
 
 function checkSecond(sec) {
